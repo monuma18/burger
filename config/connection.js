95cd6fe64512f2663af659d7ded868121ea4049b
Charles Monuma <monuma18@gmail.com>
2019-09-06 00:41:23 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -15,15 +15,8 @@ connection = mysql.createConnection({
   password: "love0101",
   database: "burgers_db"
 });
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+}
+ 
 
 // Export connection for our ORM to use.
 module.exports = connection;
